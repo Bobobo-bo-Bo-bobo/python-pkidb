dd7b02f028e44d4423c03b14b323677685119dde
Andreas Maus <maus@ypbind.de>
2015-12-20 13:17:41 +0100

    missing file

---
+++ initialize/pgsql/grant.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+ACCESSTO="
+  certificate
+  extension
+  signing_request
+  signature_algorithm
+  signature_algorithm_id_seq
+"
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $(basename $0) <database> <user>"
+  exit 1
+fi
+
+db=$1
+user=$2
+
+for table in ${ACCESSTO}; do
+  psql -c "GRANT ALL PRIVILEGES ON TABLE ${table} TO ${user};" ${db}
+done
+

715008166e825fa421d0a5a904a871aa0950c854
Andreas Maus <maus@ypbind.de>
2016-01-20 15:14:21 +0100

    Bump to 0.8.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 PKIDB_SETUP = {
     "name" : "pkidb",
-    "version" : "0.8.8",
+    "version" : "0.8.9",
     "description" : "PKI system based on a SQL database",
     "long_description" : """PKI script for managing certificates.
 Certificates are stored in a database.
@@ -15,7 +15,13 @@ SQLite3 (via python-pysqlite2)""",
     "scripts" : ["bin/pkidb"],
     "packages" : ["pkidbbackends"],
     "package_data" : {"pkidbbackends" : ["pkidbbackends/*.py"], },
-    "license" : open("LICENSE").read()
+    "license" : open("LICENSE").read(),
+    "data_files": [
+        ("share/man/man1", ["doc/man/man1/pkidb.1"]),
+        ("share/python-pkidb/initialisation/pgsql", ["initialize/pgsql/pgsql.sql", "initialize/pgsql/grant.sh"]),
+        ("share/python-pkidb/initialisation/mysql", ["initialize/mysql/mysql.sql"]),
+        ("share/python-pkidb/initialisation/sqlite", ["initialize/sqlite/sqlite.sql"]),
+    ],
 }
 
 setup(**PKIDB_SETUP)
